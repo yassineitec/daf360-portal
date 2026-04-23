3f74dcaf764b74c7c2a00e86dde1ac7d0001e2af
yassineitec <yassine.rhouma@itecgroupe.com>
2026-04-23 15:18:52 +0100

    chore: spring boot init

---
+++ src/main/java/com/daf360/portal/PortalApplication.java
@@ -0,0 +1,13 @@
+package com.daf360.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daf360/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daf360.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
