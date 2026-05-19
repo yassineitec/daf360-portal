c5b7bf330c991063b385c305d0b7da97434c9e4f
yassineitec <yassine.rhouma@itecgroupe.com>
2026-05-19 17:54:31 +0100

    feat: add Pays entity and PaysRepository for iso_code lookup

---
+++ src/main/java/com/daf360/portal/entity/Pays.java
@@ -0,0 +1,22 @@
+package com.daf360.portal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Pays")
+public class Pays {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "iso_code", length = 10)
+    private String isoCode;
+
+    @Column(name = "nom_fr", length = 100)
+    private String nomFr;
+}
---
+++ src/main/java/com/daf360/portal/repository/PaysRepository.java
@@ -0,0 +1,7 @@
+package com.daf360.portal.repository;
+
+import com.daf360.portal.entity.Pays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaysRepository extends JpaRepository<Pays, Long> {
+}
